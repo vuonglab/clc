a2ae581bf0aad8dc1ccb0fbed257d6c8fd6c32ab
Phu Vuong <pvuong@vuonglab.com>
2018-08-06 07:39:08 -0700

    Output 18-digit (not 17-digit) answers when adding and subtracting floats.

--- evaluation_result.h
+++ evaluation_result.h
@@ -10,4 +10,16 @@ struct evaluation_result {
     bool expression_contains_multiplication_or_division;
 };
 
+typedef struct trailing_nines_result trailing_nines_result;
+struct trailing_nines_result {
+	int nine_count;
+	int non_nine_count;
+};
+
+typedef struct trailing_zeros_result trailing_zeros_result;
+struct trailing_zeros_result {
+	int zero_count;
+	int non_zero_count;
+};
+
 #endif
