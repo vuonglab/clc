8fe4c5dd2f895879f4b2aacca36c4641ffda7943
Phu Vuong <pvuong@vuonglab.com>
2018-08-10 18:39:05 -0700

    Relax constraints on when trailing zeros or nines are truncated.

--- clc.c
+++ clc.c
@@ -163,14 +163,15 @@ static void pretty_print_answer(evaluation_result result)
 			trailing_d_result nines_result = get_number_of_trailing_d_followed_by_up_to_two_non_d(buffer, '9');
 			int number_of_nines = nines_result.d_count + nines_result.non_d_count;
 			// 3-10
-			if ((8 <= number_of_nines && number_of_nines <= num_decimal_places-3) || (nines_result.d_count == 4 && num_decimal_places >= 15)) {
+			if ((8 <= number_of_nines && number_of_nines <= num_decimal_places) || (nines_result.d_count == 4 && num_decimal_places >= 15)) {
 				// handle answers like -8.408039999999999 (should be -8.40804)
 				snprintf_with_exit(buffer, buf_size, "%.*Lf", num_decimal_places-number_of_nines, answer);
 			}
 			trailing_d_result zeros_result = get_number_of_trailing_d_followed_by_up_to_two_non_d(buffer, '0');
 			int number_of_zeros = zeros_result.d_count + zeros_result.non_d_count;
 			// 9-12
-			if ((9 <= number_of_zeros && number_of_zeros <= num_decimal_places-3) || (zeros_result.d_count >= 4 && num_decimal_places >= 15)) {
+			// Don't truncate -9170.0000000000029 and -508.00000000000087. 
+			if ((9 <= number_of_zeros && number_of_zeros <= num_decimal_places-1) || (zeros_result.d_count >= 4 && num_decimal_places >= 15)) {
 				// handle answers like -0.9400000000000001 (should be -0.94)
 				snprintf_with_exit(buffer, buf_size, "%.*Lf", num_decimal_places-number_of_zeros, answer);
 			}
