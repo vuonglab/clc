af1570c7b27d891b40d22b793098c4c148de2dfd
Phu Vuong <pvuong@vuonglab.com>
2018-08-15 22:50:52 -0700

    Truncate trailing zeros only if it's followed by at least one non-zero. (All trailing zeros will be truncated later on.)

--- clc.c
+++ clc.c
@@ -168,7 +168,7 @@ static void pretty_print_answer(evaluation_result result)
 			trailing_d_result zeros_result = get_number_of_trailing_d_followed_by_up_to_two_non_d(buffer, '0');
 			int number_of_zeros = zeros_result.d_count + zeros_result.non_d_count;
 			// Don't truncate -9170.0000000000029 and -508.00000000000087. 
-			if (5 <= number_of_zeros && number_of_zeros <= num_decimal_places-1)
+			if (zeros_result.non_d_count > 0 && 5 <= number_of_zeros && number_of_zeros <= num_decimal_places-1)
 				snprintf_with_exit(buffer, buf_size, "%.*Lf", num_decimal_places-number_of_zeros, answer);
 		}
 	}
